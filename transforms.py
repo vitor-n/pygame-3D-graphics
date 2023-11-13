17ffee9db01e3e640291980956ff65c6fa817d27
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2023-11-13 19:14:57 -0300

    Commented what I could understand.

--- transforms.py
+++ transforms.py
@@ -3,6 +3,10 @@ from math import sin, cos, tan, radians, atan
 
 
 def rotation_x(theta):
+    """
+    Returns a rotation matriz around the x axis
+    according to the specified theta
+    """
     r_x = np.matrix(([1,          0,          0, 0],
                      [0, cos(theta),-sin(theta), 0],
                      [0, sin(theta), cos(theta), 0],
@@ -11,6 +15,10 @@ def rotation_x(theta):
     return r_x
 
 def rotation_y(theta):
+    """
+    Returns a rotation matriz around the x axis
+    according to the specified theta
+    """
     r_x = np.matrix(([cos(theta), 0, -sin(theta), 0],
                      [         0, 1,           0, 0],
                      [sin(theta), 0,  cos(theta), 0],
@@ -19,6 +27,10 @@ def rotation_y(theta):
     return r_x 
 
 def rotation_z(theta):
+    """
+    Returns a rotation matriz around the x axis
+    according to the specified theta
+    """
     r_x = np.matrix(([cos(theta),-sin(theta), 0, 0],
                      [sin(theta), cos(theta), 0, 0],
                      [         0,          0, 1, 0],
@@ -27,7 +39,10 @@ def rotation_z(theta):
     return r_x
 
 def translation(tx, ty, tz):
-
+    """
+    Receives the coordinates of the nearest face of an object.
+    Returns the translation matrix for said object.
+    """
     t_matrix = np.identity(4)
 
     t_matrix[0,3] = tx
@@ -37,7 +52,13 @@ def translation(tx, ty, tz):
     return t_matrix
 
 def perspective(fov:float, aspect:float, znear:float, zfar:float):
+    """
+    Receives the angle of the field of view, 
+    the aspect ratio of the plane (normally the screen)
+    znear and zfar, which are the distances between the nearest face and the furthest one
 
+    Returns the perspective projection matrix
+    """
     m = np.zeros((4,4))
 
     e = 1 / tan(radians(fov/2))
@@ -55,6 +76,10 @@ def perspective(fov:float, aspect:float, znear:float, zfar:float):
     return test
 
 def camera_translation(pos):
+    """ ?
+    Receives the coordinates of the nearest face of an object.
+    Returns the translation matrix for said object.
+    """
     camera_matrix = np.matrix([[1,0,0,-pos[0]],
                                [0,1,0,-pos[1]],
                                [0,0,1,-pos[2]],
@@ -64,7 +89,14 @@ def camera_translation(pos):
     return camera_matrix
 
 def camera_orientation(u,v,n):
+    """
+    Receives three vectors, representing:
+    the direction the viewer is facing
+    their horizontal
+    their vertical
 
+    Returns the orientation_matrix
+    """
     orientation_matrix = np.matrix(([u[0,0], u[0,1], u[0,2], 0],
                                     [v[0,0], v[0,1], v[0,2], 0],
                                     [n[0,0], n[0,1], n[0,2], 0],
@@ -73,7 +105,8 @@ def camera_orientation(u,v,n):
     return orientation_matrix
 
 def viewport(width, height):
-
+    """ ?
+    """
     view = np.matrix(([width//2, 0,0,0],
                       [0,height//2,0,0],
                       [0,0,1/2,1/2],

2e2ee42be173400ba2ac76dc6b9d22fc84f6db02
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2023-11-22 11:40:37 -0300

    FIXED!!!!!!!!!!!!!!!!!!!!!

--- axes_indicator.py
+++ axes_indicator.py
@@ -14,16 +14,13 @@ class AxesIndicator:
 
     def draw(self, screen: pygame.Surface):
         vectors_matrix = np.concatenate((self.vec_x, self.vec_y, self.vec_z))
-        projection_matrix = np.array(([1, 0, 0], [0, 1, 0]))
-        add_center_matrix = np.array(([self.center[0], self.center[1]], 
-                                      [self.center[0], self.center[1]], 
-                                      [self.center[0], self.center[1]]))
+        
+        add_center_matrix = np.array(([self.center[0], self.center[1], 0], 
+                                      [self.center[0], self.center[1], 0], 
+                                      [self.center[0], self.center[1], 0]))
+        
+        lines_end_positions = (vectors_matrix * self.line_size + add_center_matrix).tolist()
 
-        projected_matrix = projection_matrix @ vectors_matrix
-        print(projected_matrix)
-        transposed_matrix = (projected_matrix * self.line_size).transpose()
-        lines_end_positions = (transposed_matrix + add_center_matrix).tolist()
-
-        pygame.draw.line(screen, "red", self.center, lines_end_positions[0], 2)
-        pygame.draw.line(screen, "green", self.center, lines_end_positions[1], 2)
-        pygame.draw.line(screen, "blue", self.center, lines_end_positions[2], 2)
+        pygame.draw.line(screen, "red", self.center, lines_end_positions[0][:2], 2)
+        pygame.draw.line(screen, "green", self.center, lines_end_positions[1][:2], 2)
+        pygame.draw.line(screen, "blue", self.center, lines_end_positions[2][:2], 2)
